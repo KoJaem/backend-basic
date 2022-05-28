0a9d98a61a82b63614db0e4e155f203aaf20dd5d
KoJaem <woalswhwh@naver.com>
2022-05-29 03:40:52 +0900

    6. REST API 에 대한 공부

--- backend/app.js
+++ backend/app.js
@@ -17,6 +17,9 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+
+
+
 app.get("/database", function (req, res) {
   res.send(database);
 });
@@ -32,23 +35,30 @@ app.get("/database/:id", function (req, res) {
 
 
 
-// params 를 통해 글 추가(요청)를 받는방법 (많이 사용하지는 않음.)
-// : 을 쓰고 뒤에 어떤값(지금은 title)을적으면
-// : 뒤에 있는 url 은 title 로 보겠다 라는 뜻임.
-app.get("/database/:title", function (req, res) {
-  const title = req.params.title;
-  database.push({
-    id: database.length + 1,
-    title,
-  });
-  res.send("값 추가가 정상적으로 완료되었습니다.");
-});
+// // params 를 통해 글 추가(요청)를 받는방법 (많이 사용하지는 않음.)
+// // : 을 쓰고 뒤에 어떤값(지금은 title)을적으면
+// // : 뒤에 있는 url 은 title 로 보겠다 라는 뜻임.
+// app.get("/database/:title", function (req, res) {
+//   const title = req.params.title;
+//   database.push({
+//     id: database.length + 1,
+//     title,
+//   });
+//   res.send("값 추가가 정상적으로 완료되었습니다.");
+// });
+
+
+// REST API
+// 생성 : POST
+// 수정 : PUT, PATCH
+// 삭제 : DELETE
+
 
 // ★ body 를 통해 글 추가(요청)를 받는방법
 // get 메소드 (X) / post 메소드 (O)
 // body 에 담아오는거를 읽어오려면
 // bodyParser 라는것을 express 에 추가해줘야함. (위쪽에 있음)
-app.post("/add-database", function (req, res) {
+app.post("/database", function (req, res) {
   const title = req.body.title;
   database.push({
     id: database.length + 1,
@@ -58,7 +68,7 @@ app.post("/add-database", function (req, res) {
 });
 
 // 글 수정
-app.post("/update-database", function (req, res) {
+app.put("/database", function (req, res) {
   const id = req.body.id;
   const title = req.body.title;
   database[id-1].title = title;
@@ -66,7 +76,7 @@ app.post("/update-database", function (req, res) {
 });
 
 // 글 삭제
-app.post("/delete-database", function (req, res) {
+app.delete("/database", function (req, res) {
   const id = req.body.id;
   // splice 메소드 : 어떤 값을 삭제하고 싶은 시작 지점 인덱스부터 n개를 삭제해주는 메소드
   database.splice(id - 1, 1);
