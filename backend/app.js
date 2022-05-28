fb690f3ee2e7258d44e2e7c60ec1a0931c31babf
KoJaem <woalswhwh@naver.com>
2022-05-29 03:15:13 +0900

    3. 글 요청 api 공부

--- backend/app.js
+++ backend/app.js
@@ -21,6 +21,13 @@ app.get("/database", function (req, res) {
   res.send(database);
 });
 
+app.get("/database/:id", function (req, res) {
+  // req 로 받아온 id 는 String 이므로 Number 로 바꿔줘야함
+  const id = req.params.id;
+  const data = database.find(el => el.id === Number(id));
+  res.send(data);
+});
+
 // params 를 통해 글 추가(요청)를 받는방법 (많이 사용하지는 않음.)
 // : 을 쓰고 뒤에 어떤값(지금은 title)을적으면
 // : 뒤에 있는 url 은 title 로 보겠다 라는 뜻임.
