a50d0269971253c4b46b84297afc132c183af927
KoJaem <woalswhwh@naver.com>
2022-05-30 04:19:43 +0900

    로그인 공부

--- backend/app_user.js
+++ backend/app_user.js
@@ -34,6 +34,26 @@ app.post("/signup", (req, res) => {
   res.send("success");
 });
 
+// 로그인 코드
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  const user = database.filter(user => {
+    return user.username === username;
+  });
+  if (user.length === 0) {
+    res.send("해당하는 id가 없습니다. ");
+    return;
+  }
+
+  // username 이 중복이 되지않는다는 가정하에 user[0] 사용
+  if (user[0].password !== password) {
+    res.send("패스워드가 틀립니다.");
+    return;
+  }
+
+  res.send("로그인 성공!");
+});
+
 app.listen(3000, () => {
   console.log("server on!");
 });
